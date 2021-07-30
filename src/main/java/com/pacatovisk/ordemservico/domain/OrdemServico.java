2d9a3622ae2cfdc760bd134af2ac3942fe0502e9
pacatovisk <marcelovianadias@outlook.com>
2021-07-30 15:38:32 -0300

    Implementando Classe OrdemServico

---
+++ src/main/java/com/pacatovisk/ordemservico/domain/OrdemServico.java
@@ -0,0 +1,95 @@
+package com.pacatovisk.ordemservico.domain;
+
+import com.pacatovisk.ordemservico.domain.enums.Prioridade;
+import com.pacatovisk.ordemservico.domain.enums.Status;
+
+import java.time.LocalDateTime;
+
+public class OrdemServico {
+
+    private Integer id;
+    private LocalDateTime  dataAbertura;
+    private LocalDateTime dataFechamento;
+    private Integer prioridade;
+    private String observacoes;
+    private Integer status;
+    private Tecnico tecnico;
+    private Cliente cliente;
+
+    public OrdemServico(){
+        super();
+        this.setDataAbertura(LocalDateTime.now());
+        this.setPrioridade(Prioridade.BAIXA);
+        this.setStatus(Status.ABERTO);
+    }
+
+    public OrdemServico(Integer id, Prioridade prioridade, String observacoes, Status status, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.setDataAbertura(LocalDateTime.now());
+        this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+        this.observacoes = observacoes;
+        this.status = (status == null) ? 0 : status.getCod();
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDateTime getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDateTime dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public Prioridade getPrioridade() throws IllegalAccessException {
+        return Prioridade.toEnum(this.prioridade);
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade.getCod();
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Status getStatus() throws IllegalAccessException {
+        return Status.toEnum(this.status);
+    }
+
+    public void setStatus(Status status) {
+        this.status = status.getCod();
+    }
+
+    public Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
